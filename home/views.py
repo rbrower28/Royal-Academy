9ff098696731a5a6c035bb39fea9b6b44c879931
Ryan Brower <ryan.brower28@gmail.com>
2022-05-20 11:26:11 -0700

    static & template folders

--- home/views.py
+++ home/views.py
@@ -4,5 +4,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello World!")
+
+    return render(request, 'home/index.html')
+
+    # returns the html file found at
+    # 'academy/home/templates/home/index.html'
+
+    # Django searches through every folder called
+    # 'templates' to find the page requested
+
+    # putting html files in a subfolder with the same
+    # name as the module will keep index files separate
