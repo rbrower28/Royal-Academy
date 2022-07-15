3d335dd90120db69b3c57378503f4f495ecb58dc
Mathieu Steele <matt@steeleagency.com>
2022-07-14 21:20:46 -0400

    add comments to quizzes app

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Quiz,Question,QuestionOption,QuizAttempt,QuestionAttempt
 
+# Make sure each of the models of the app are visible in the admin.
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(QuestionOption)
--- quizzes/urls.py
+++ quizzes/urls.py
@@ -5,8 +5,12 @@ from . import views
 app_name = 'quizzes'
 
 urlpatterns = [
+    # base route shows the list of quizzes
     path('', views.QuizListView.as_view(), name='quiz-list'),
+    # route for taking a quiz
     path('quiz/<str:id>/take', views.take_quiz, name='take_quiz'),
+    # route for submitting a quiz
     path('quiz/<str:id>/submit_attempt', views.submit_quiz_attempt, name='submit_quiz_attempt'),
+    # route for showing the results of a quiz
     path('results/<str:id>', views.quiz_results, name='quiz_results'),
 ]
