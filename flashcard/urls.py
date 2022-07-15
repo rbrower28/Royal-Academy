baeaf45779439230fa1d917881b22d7b96892cea
dandlp95 <del17011@byui.edu>
2022-07-14 19:12:56 -0600

    fixed flashcard menu url

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = 'flashcards'
 
 urlpatterns = [
-    path("", views.FlashcardListView.as_view(), name="flashcard-list"),
-    path("flashcard-menu", views.flashcards_menu, name="flashcard-menu"),
+    path("", views.flashcards_menu, name="flashcard-menu"),
     path("play/<str:id>", views.play_flashcards, name="play_flashcards"),
     path("add-deck", views.create_deck, name="create_deck"),
     path("add-flashcard", views.create_flashcard, name="create_flashcard"),
